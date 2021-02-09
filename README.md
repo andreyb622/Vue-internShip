a41abad055b9575e3135cd838c93e74d95f43d94
andreyb622 <andreyb622@gmail.com>
2021-02-09 12:27:58 +0300

    rename

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Vue-internShip
-intenship lecture code
+intenship task code
---
+++ task2/src/components/CreateContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+    <div>
+      <label>Name</label>
+      <input type="text" />
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" />
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" />
+    </div>
+    <button>
+      add contact
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "new-contact", // необезательное свойство 
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
