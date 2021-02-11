a7465ba305ec9d989631bf9e72f934e8d1666fda
andreyb622 <andreyb622@gmail.com>
2021-02-11 17:32:01 +0300

    done

---
+++ task3/src/components/CreateContact.vue
@@ -0,0 +1,44 @@
+<template>
+  <form>
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="name"/>
+    </div>
+    <div>
+      <label>Phone</label>
+      <input type="tel" v-model="phone"/>
+    </div>
+    <div>
+      <label>Email</label>
+      <input type="email" v-model="email"/>
+    </div>
+    <button @click.prevent="addNewContact">
+      add contact
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "new-contact", // необезательное свойство 
+  emits: ["addNewContact"],
+  data() {
+    return {
+      name: '',
+      phone: '',
+      email: ''
+    };
+  },
+  methods: {
+    addNewContact() {
+      this.$emit("addNewContact", 
+        this.name,
+        this.phone,
+        this.email,
+      )
+    }
+  },
+};
+</script>
+
+<style></style>
